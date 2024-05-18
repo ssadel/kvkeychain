35ba72054e5bdd240e2cd69bd2a1b3860ceace07
Sid Sadel <sidneysadel@gmail.com>
2024-05-18 15:36:36 -0400

    make kvkeychain a struct

--- Sources/Keychain/KVKeychain.swift
+++ Sources/Keychain/KVKeychain.swift
@@ -2,18 +2,11 @@ import Foundation
 import Security
 
 /// Encapsulate Keychain functionality in a class similar to UserDefaults
-public final class KVKeychain {
-    /// Returns the standard keychain object.
-    static let standard: KVKeychain = .init()
-    
-    internal init() {
-        // no op
-    }
-    
+public struct KVKeychain {
     /// Sets the value of the specified default key.
-    public func set<T: Codable>(_ value: T?, forKey: String) throws {
+    public static func set<T: Codable>(_ value: T?, forKey: String) throws {
         if value == nil {
-            try self.delete(forKey: forKey)
+            try delete(forKey: forKey)
             return
         }
         
@@ -21,7 +14,7 @@ public final class KVKeychain {
         do {
             data = try JSONEncoder().encode(value)
         } catch {
-            throw Self.KVKeychainError.failedToEncodeValue(error)
+            throw KVKeychainError.failedToEncodeValue(error)
         }
         
         let query: [String: Any] = [
@@ -32,16 +25,16 @@ public final class KVKeychain {
         let status = SecItemAdd(query as CFDictionary, nil)
         if status == errSecDuplicateItem {
             print("🔑 KVKeychain: Value exists, attempting to update...")
-            try self.update(data, forKey: forKey)
+            try update(data, forKey: forKey)
             return
         }
         guard status == errSecSuccess else {
-            throw Self.KVKeychainError.unknown(status)
+            throw KVKeychainError.unknown(status)
         }
     }
     
     /// Gets the value for the given key. Returns nil if no value is found for the key
-    public func getValue<T: Decodable>(forKey: String) throws -> T? {
+    public static func getValue<T: Decodable>(forKey: String) throws -> T? {
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: forKey,
@@ -64,11 +57,11 @@ public final class KVKeychain {
             let value = try JSONDecoder().decode(T.self, from: data)
             return value
         } catch {
-            throw Self.KVKeychainError.failedToDecodeValue(error)
+            throw KVKeychainError.failedToDecodeValue(error)
         }
     }
     
-    public func update(_ value: Data, forKey: String) throws {
+    public static func update(_ value: Data, forKey: String) throws {
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: forKey
@@ -76,18 +69,18 @@ public final class KVKeychain {
         let attributes: [String: Any] = [kSecValueData as String: value]
         let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
         guard status == noErr else {
-            throw Self.KVKeychainError.failedToUpdateValue(status)
+            throw KVKeychainError.failedToUpdateValue(status)
         }
     }
     
-    internal func delete(forKey: String) throws {
+    internal static func delete(forKey: String) throws {
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: forKey
         ]
         let status = SecItemDelete(query as CFDictionary)
         guard status == noErr else {
-            throw Self.KVKeychainError.failedToDeleteValue(status)
+            throw KVKeychainError.failedToDeleteValue(status)
         }
     }
 }
